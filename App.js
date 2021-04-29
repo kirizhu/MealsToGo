e7ff060727fb7224ac794ff8cf790700bbbc938e
Eddy Garcia <eddy.garcia@staylive.se>
2021-04-29 10:40:34 +0200

    setting up linter

--- App.js
+++ App.js
@@ -1,39 +1,37 @@
-import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View , SafeAreaView , StatusBar} from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
 
 export default function App() {
   return (
     <>
-    <SafeAreaView style={styles.container}>
-      <View style={styles.search}>
-      <Text style={styles.text}>Search</Text>
-      </View>
-      <View style={styles.list}>
-      <Text style={styles.text}>List</Text>
-      </View>
-    </SafeAreaView>
-      <ExpoStatusBar style='auto'/>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text style={styles.text}>Search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text style={styles.text}>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    marginTop: StatusBar.currentHeight, 
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
   },
-  search:{
-    backgroundColor:'green',
-    justifyContent:'center',
-    padding: 16
+  search: {
+    backgroundColor: "green",
+    justifyContent: "center",
+    padding: 16,
   },
-  list:{
-    flex : 1,
-    backgroundColor:'blue',
-    padding: 16
+  list: {
+    flex: 1,
+    backgroundColor: "blue",
+    padding: 16,
   },
-  text:{
-
-  }
+  text: {},
 });
